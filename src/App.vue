322179bd9d58e200fdfb92b32b5deaccb4f95179
Dan1LExtr1m <daniilevdokimov29@gmail.com>
2024-11-28 04:44:40 +0300

    feat: create a custom titlebar and a theme switch

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import TitleBar from './components/TitleBar.vue'
 </script>
 
 <template>
-  <main class="font-roboto container">
-    <h1 class="font-quicksand font-bold">Future Alchemy will be here.</h1>
-    <Button>Click</Button>
+  <TitleBar />
+  <main class="font-roboto container pt-24">
+    <h1 class="flex justify-center font-quicksand font-bold text-5xl select-none">Alchemy</h1>
+    <p class="flex justify-center pt-4 text-base text-center text-slate-900 dark:text-slate-200 select-none">Welcome back! What would you like to do today?</p>
   </main>
 </template>
