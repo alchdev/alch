b2d69be7f1e3a2a58789a691dfd3d0769588d466
cheasea <email@email.com>
2024-11-29 16:23:39 +0200

    chore: fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginVue from 'eslint-plugin-vue';
-import globals from 'globals';
-import typescriptEslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import typescriptEslint from 'typescript-eslint'
 
-export default eslint.config(
+export default typescriptEslint.config(
   { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
   {
     extends: [
       eslint.configs.recommended,
       ...typescriptEslint.configs.recommended,
-      ...eslintPluginVue.configs['flat/recommended']
+      ...eslintPluginVue.configs['flat/recommended'],
     ],
     files: ['**/*.{ts,vue}'],
     languageOptions: {
@@ -18,12 +18,12 @@ export default eslint.config(
       sourceType: 'module',
       globals: globals.browser,
       parserOptions: {
-        parser: typescriptEslint.parser
-      }
+        parser: typescriptEslint.parser,
+      },
     },
     rules: {
       // your rules
-    }
+    },
   },
-  eslintConfigPrettier
-);
+  eslintConfigPrettier,
+)
