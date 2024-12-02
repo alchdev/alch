9da70925634374a189b1f6a2dcb50eac7d2356c0
Winston <37739893+cheasea@users.noreply.github.com>
2024-12-02 22:23:39 +0200

    build: add vue router

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,5 @@ import TitleBar from './components/TitleBar.vue'
 
 <template>
   <TitleBar />
-  <main class="font-roboto container pt-24">
-    <h1
-      class="flex justify-center font-quicksand font-bold text-5xl select-none"
-    >
-      Alchemy
-    </h1>
-    <p
-      class="flex justify-center pt-4 text-base text-center text-slate-900 dark:text-slate-200 select-none"
-    >
-      Welcome back! What would you like to do today?
-    </p>
-  </main>
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="font-roboto container pt-24">
+    <h1
+      class="flex justify-center font-quicksand font-bold text-5xl select-none"
+    >
+      Alchemy
+    </h1>
+    <p
+      class="flex justify-center pt-4 text-base text-center text-slate-900 dark:text-slate-200 select-none"
+    >
+      Welcome back! What would you like to do today?
+    </p>
+  </main>
+</template>
