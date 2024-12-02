25dde8bbf27a38e579d373ccd21f36e3706dafc9
Winston <37739893+cheasea@users.noreply.github.com>
2024-12-02 20:30:32 +0200

    chore: dont format & lint components/ui/**, vite-env.d.ts

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,15 @@ import globals from 'globals'
 import typescriptEslint from 'typescript-eslint'
 
 export default typescriptEslint.config(
-  { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
+  {
+    ignores: [
+      '*.d.ts',
+      '**/coverage',
+      '**/dist',
+      'src/components/ui/**',
+      'vite-env.d.ts',
+    ],
+  },
   {
     extends: [
       eslint.configs.recommended,
